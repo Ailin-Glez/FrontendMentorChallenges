21997381035fbf0fbf1a3ef3fe820da5ba5273a8
Ailin Garcia <agarcia@switch.com.uy>
2023-02-24 10:21:48 -0300

    logic improvement

--- interactive-rating/src/App.jsx
+++ interactive-rating/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react';
 
-import Card from './Card';
+import Rating from './Rating';
+import ThankYou from './ThankYou';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [numberSelected, setNumberSelected] = useState(null);
+
+  const handleNumberSelected = (number) => {
+    setNumberSelected(number);
+  };
+
+  const handleSubmit = () => {
+    setIsSubmitted((submitted) => !submitted);
+  };
 
   return (
-    <div className="App">
-      <Card />
+    <div className="card">
+      {isSubmitted ? (
+        <ThankYou number={numberSelected} />
+      ) : (
+        <>
+          <img src="./icon-star.svg" alt="star image" className="star" />
+          <div className="text">
+            <h1>How did we do?</h1>
+            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+          </div>
+          <Rating handleNumberSelected={handleNumberSelected} />
+          <button className="submit-btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </>
+      )}
     </div>
   );
 }
--- interactive-rating/src/ThankYou.jsx
+++ interactive-rating/src/ThankYou.jsx
@@ -6,7 +6,7 @@ function ThankYou({ number }) {
         <p className="selection">You selected {number} out of 5</p>
       </div>
       <h2>Thank you!</h2>
-      <p className="final-text">We appreciate you taking the time to give a rating. If you ever need more support, don´t hesitate to get in touch!</p>
+      <p className="final-text">We appreciate you taking the time to give a rating. If you ever need more support, don not hesitate to get in touch!</p>
     </div>
   );
 }
